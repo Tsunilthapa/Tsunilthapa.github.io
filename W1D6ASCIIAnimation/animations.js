75cd95d14247277266446816eb5ebbfb723766e3
sunil <tsunilthapa8@gmail.com>
2021-10-03 17:22:00 -0500

    lint validator

--- W1D6ASCIIAnimation/animations.js
+++ W1D6ASCIIAnimation/animations.js
@@ -15,6 +15,7 @@
 // PUT YOUR CODE IN THERE.  In myanimation.js, it is legal for you to modify
 // the variables that have been declared in this file.
 
+/*jshint sub:true*/
 var BLANK = "";
 
 var CUSTOM = "";   // you could change the value of this variable in your code
@@ -641,9 +642,20 @@ var DIVE = "  o\n" +
 // can also access the animations as an associative array / hash map,
 // such as ANIMATIONS["Bike"] or ANIMATIONS["Dive"]
 var ANIMATIONS = [];
-ANIMATIONS["Blank"] = ANIMATIONS["blank"] = ANIMATIONS["BLANK"] = "";
-ANIMATIONS["Custom"] = ANIMATIONS["custom"] = ANIMATIONS["CUSTOM"] = CUSTOM;   // you could change the value of this in your code
-ANIMATIONS["Exercise"] = ANIMATIONS["exercise"] = ANIMATIONS["EXERCISE"] = EXERCISE;
-ANIMATIONS["Juggler"] = ANIMATIONS["juggler"] = ANIMATIONS["JUGGLER"] = JUGGLER;
-ANIMATIONS["Bike"] = ANIMATIONS["bike"] = ANIMATIONS["BIKE"] = BIKE;
-ANIMATIONS["Dive"] = ANIMATIONS["dive"] = ANIMATIONS["DIVE"] = DIVE;
+
+
+// ANIMATIONS["Blank"] = ANIMATIONS["blank"] = ANIMATIONS["BLANK"] = BLANK = "";
+// ANIMATIONS["Custom"] = ANIMATIONS["custom"] = ANIMATIONS["CUSTOM"] = CUSTOM;   // you could change the value of this in your code
+// ANIMATIONS["Exercise"] = ANIMATIONS["exercise"] = ANIMATIONS["EXERCISE"] = EXERCISE;
+// ANIMATIONS["Juggler"] = ANIMATIONS["juggler"] = ANIMATIONS["JUGGLER"] = JUGGLER;
+// ANIMATIONS["Bike"] = ANIMATIONS["bike"] = ANIMATIONS["BIKE"] = BIKE;
+// ANIMATIONS["Dive"] = ANIMATIONS["dive"] = ANIMATIONS["DIVE"] = DIVE;
+
+
+
+ANIMATIONS.Blank = ANIMATIONS.blank = ANIMATIONS.BLANK = BLANK = "";
+ANIMATIONS.Custom = ANIMATIONS.custom = ANIMATIONS.CUSTOM = CUSTOM;   // you could change the value of this in your code
+ANIMATIONS.Exercise = ANIMATIONS.exercise = ANIMATIONS.EXERCISE = EXERCISE;
+ANIMATIONS.Juggler = ANIMATIONS.juggler = ANIMATIONS.JUGGLER = JUGGLER;
+ANIMATIONS.Bike = ANIMATIONS.bike = ANIMATIONS.BIKE = BIKE;
+ANIMATIONS.Dive = ANIMATIONS.dive = ANIMATIONS.DIVE = DIVE;
